4d026320e4efd29a407ed1db43e3a8855a6f79e4
Vanessa Arruda <wanessaarruda@gmail.com>
2020-10-23 08:10:30 -0600

    Add game_stats test

---
+++ test/game_stats_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/game_stats'
+require './data/dummy.csv'
+
+class GameStatsTest < Minitest::Test
+
+  def setup
+
+  end
+
+  def test_it_exists_and_has_attributes
+  end
+  
+end

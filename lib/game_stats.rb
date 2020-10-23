2739b50b18dbb0daab53982615ee4cd2cd5ff22b
Vanessa Arruda <wanessaarruda@gmail.com>
2020-10-23 08:33:18 -0600

    Add lines aplit and line split methods

--- lib/game_stats.rb
+++ lib/game_stats.rb
@@ -1,11 +1,22 @@
 class GameStats
 
   def initialize
-
+    @data_titles = []
   end
 
   def read_file(filename)
     lines = File.readlines(filename)
+    require "pry"; binding.pry
+  end
+
+  def lines_split(lines)
+    split_lines = lines.map do |line|
+      line.line_split
+    end
+  end
+
+  def line_split(line)
+    line.split(",")
   end
 
 end

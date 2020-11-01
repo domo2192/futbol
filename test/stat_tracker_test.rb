d81e33d9f9591829ef09b271d8277add45b0f6a7
Joseph Budina <joebud3@gmail.com>
2020-11-01 16:32:05 -0700

    Add most_goals_scored and fewest_goals_scored tests

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -149,4 +149,12 @@ require './lib/stat_tracker'
     def test_average_win_percentage
       assert_equal 0.49, @stat_tracker.average_win_percentage(6)
     end
+
+    def test_most_goals_scored
+      assert_equal 7, @stat_tracker.most_goals_scored(18)
+    end
+
+    def test_fewest_goals_scored
+      assert_equal 0, @stat_tracker.fewest_goals_scored(18)
+    end
   end

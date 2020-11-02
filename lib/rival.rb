1e94d079a3180729d58693707e27e378dc68292e
Max Ribbans <ribbans.max@gmail.com>
2020-11-02 17:54:26 -0500

    Fix spacing

--- lib/rival.rb
+++ lib/rival.rb
@@ -1,10 +1,9 @@
 class Rival
-  attr_reader:team_id, :opponent_id, :result
+  attr_reader :team_id, :opponent_id, :result
+
   def initialize(team_id, opponent_id, result)
     @team_id = team_id
     @opponent_id = opponent_id
     @result = result
   end
-
-  
 end
